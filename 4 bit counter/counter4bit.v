1fd895258a833d71dbe2b4c9e12a893ea0663419
talg1996 <111954825+talg1996@users.noreply.github.com>
2023-07-17 20:22:09 +0300

    Update counter4bit.v

--- 4 bit counter/counter4bit.v
+++ 4 bit counter/counter4bit.v
@@ -1,12 +1,12 @@
 module FourBitCounter(
   input wire clk,
-  input wire reset,
+  input wire reset, 
   output reg [3:0] count
 );
   always @(posedge clk or posedge reset) begin
     if (reset)
-      count <= 4'b0000;
+      count <= 4'b0000; // Set the output to zero
     else
-      count <= count + 1;
+      count <= count + 1; //Increase by 1 
   end
 endmodule
